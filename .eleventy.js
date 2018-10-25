ec80092a8ba7b21161cc505fe3cf4e17e2a4a50c
stevejamesson <steve@evendev.com>
2018-10-25 14:03:50 -0400

    product grid

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,12 @@ module.exports = config => {
   }
 
   config.addPassthroughCopy('src/files')
+  config.addPassthroughCopy('src/icons')
   config.addPassthroughCopy('src/img')
+
+  config.addCollection('tillandsias', (collection) => {
+    return collection.getFilteredByGlob('src/tillandsias/*.md')
+  })
 
   return config
 }
---
+++ src/tillandsias.md
@@ -0,0 +1,9 @@
+---
+title: Tillandsia Air Plants
+description: Hanging Air Plants
+layout: /layouts/products.pug
+pagination:
+  data: collections.tillandsias
+  size: 99999
+  alias: product
+---
